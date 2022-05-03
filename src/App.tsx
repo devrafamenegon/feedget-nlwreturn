0c95b1d00d5a0dd09087842f4ae7f9652e37cca0
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-05-02 22:12:46 -0300

    :sparkles: Creating Feedback widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-interface ButtonProps {
-  text?: string;
-}
+import { Widget } from "./components/Widget"
 
-function Button(props: ButtonProps) {
-  return <button>{props.text ?? "Default"}</button>
-}
-
-
-function App() {
-  return (
-    <>
-      <Button text="Enviar" />
-    </>
-  )
+export function App() {
+  return <Widget />
 }
 
 export default App
